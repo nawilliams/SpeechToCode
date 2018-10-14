8eb9c1911185fbbf963addcd653ca154d530a39f
Nicholas Williams <nawilliams@umass.edu>
2018-10-14 05:16:36 -0400

    add some commands

--- main.py
+++ main.py
@@ -1,34 +1,69 @@
 from pyswip import Prolog, registerForeign
 import speech_recognition as sr
+from subprocess import call
 
-def hello(t):
-    print("Hello,", t)
-hello.arity = 1
-
-registerForeign(hello)
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 prolog = Prolog()
 prolog.consult("hack.pl")
 
 
+def reset():
+    f = open("program.rb", "w")
+    f.write("")
+    f.close()
+
+def run():
+    print("""
+#################
+### Executing ###
+#################
+    """)
+    call(["ruby", "program.rb"])
+    print("""
+#################
+### Stopping  ###
+#################
+    """)
+
+
 def parse(words):
+
     query = '["' + '", "'.join(words.split(" ")) + '"]'
-    print(list(prolog.query('parse(' + query + ', X), hello(X)')))
+    out = list(prolog.query('parse(' + query + ', X)'))
+    if not out:
+        print(colors.WARNING + "failed to parse")
+        return ""
+    return " ".join(map(lambda x: x.decode('ascii'), out[0]["X"]))
 
 r = sr.Recognizer()
 while(True):
     with sr.Microphone() as source:
         print("adjusting for ambient noise")
         r.adjust_for_ambient_noise(source)
-        print("recording")
+        print(colors.OKBLUE + "recording")
         audio = r.listen(source)
-        print("finished recording")
+        print(colors.OKBLUE + "finished recording")
         try:
             a = r.recognize_google(audio).lower()
-            print(a)
-            parse(a)
+            print(colors.OKGREEN + a)
+            if a == "reset" or a == "erase":
+                reset()
+            if a == "run" or a == "execute":
+                run()
+            f = open("program.rb", "a")
+            f.write(parse(a))
+            f.close()
 
         except sr.UnknownValueError:
-            print("couldn't understand")
+            print(colors.FAIL + "couldn't understand")
         except sr.RequestError as e:
-            print("failed: {0}".format(e))
+            print(cololrs.FAIL + "failed: {0}".format(e))
